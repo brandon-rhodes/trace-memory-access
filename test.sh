5925add43c5d59f9e8424dab5713913e507b754a
Brandon Rhodes <brandon@rhodesmill.org>
2015-01-07 02:03:33 -0500

    Use awk to grab only interesting part of trace

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SDE=~/sde-external-7.8.0-2014-10-02-lin/sde
+
 if [ ! -f stage/bin/python2.7 ] ;then
     mkdir -p stage/bin
     mkdir -p stage/lib/python2.7
@@ -9,4 +11,5 @@ fi
 
 cp site.py stage/lib/python2.7/site.py
 
-exec ~/sde-external-7.8.0-2014-10-02-lin/sde -debugtrace -- stage/bin/python2.7
+exec $SDE -odebugtrace /dev/stdout -- stage/bin/python2.7 |
+awk '/0x280de81/,/0x280de82/' > trace.out
